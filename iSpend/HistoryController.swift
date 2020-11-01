88ab3d06226c085687c16b4b077ef8a7113aa554
Jaroslav Hampejs <hampejar@fit.cvut.cz>
2020-11-01 20:42:29 +0100

    fetching and displaying transactions at a basic level works hurray

--- iSpend/HistoryController.swift
+++ iSpend/HistoryController.swift
@@ -8,13 +8,37 @@
 import UIKit
 import FirebaseDatabase
 
-class HistoryController: UIViewController {
+class HistoryController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var transactionsCollectionView: UICollectionView!
     
     let dbRef = Database.database().reference()
+    var transactions: NSArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        transactionsCollectionView.delegate = self
+        transactionsCollectionView.dataSource = self
         
+        dbRef.child("transactions").observeSingleEvent(of: .value) { [self] (snapshot) in
+            transactions = snapshot.value as? NSArray ?? []
+            //for transaction in self.transactions {
+            //    print("trans: \(transaction)\n")
+            //}
+            transactionsCollectionView.reloadData()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return transactions.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TransactionViewCell", for: indexPath) as! TransactionViewCell
         
+        let transaction = transactions[indexPath.row] as! NSDictionary
+        cell.label.text = transaction["title"] as? String
+        
+        return cell
     }
 }
---
+++ iSpend/TransactionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionViewCell.swift
+//  iSpend
+//
+//  Created by Jaroslav Hampejs on 01/11/2020.
+//
+
+import UIKit
+
+class TransactionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+}
